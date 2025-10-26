9b9550e3adc0b15a1561d564ef9673862b230fe8
Gautier François <238468263+atturbin@users.noreply.github.com>
2025-10-26 14:07:54 +0100

    feat(exp): better file structure and assertions

--- 1-strings/main.d
+++ 1-strings/main.d
@@ -1,32 +1,60 @@
 import std.stdio;
+import std.string;
+import helpers.xhelpers;
+
+void immutable_string()
+{
+    string example = "immutable string";
+    debug_write("immutable_string", example);
+}
+
+void immutable_string_forloop()
+{
+    char[] example = "immutable string".dup;
+    for (int i = 0; i < example.length; i++)
+    {
+        debug_write(format("immutable_string_forloop - %d", i), example[i]);
+    }
+}
+
+void mutable_char_from_immutable()
+{
+    string example = "immutable string";
+    char[] mutable;
+
+    foreach (character; example)
+    {
+        mutable ~= character;
+    }
+
+    mutable[0] = 'b';
+    assert(mutable == "bmmutable string");
+    debug_write("mutable_char_from_immutable", mutable);
+}
+
+void mutable_char_from_dup()
+{
+    string example = "immutable string";
+    char[] copy = example.dup();
+
+    copy[0] = 'c';
+
+    assert(copy == "cmmutable string");
+    debug_write("mutable_char_from_dup", copy);
+}
 
 void main()
 {
-    writeln("hello from strings");
+    // strings are immutable
+    immutable_string();
+    // you can iterate over strings as they are an alias for char[].
+    immutable_string_forloop();
 
-    string name = "atturbin";
+    // as they are immutable, you need to iterate over them
+    // to populate a new mutable char[].
+    mutable_char_from_immutable();
 
-    for (int i = 0; i < name.length; i++)
-    {
-        write("char: ", i, " = '", name[i], "' | ");
-    }
-
-    // strings aren't mutable
-    // we can iterate over the elements to populate a mutable char[].
-    char[] mutable_name = [];
-    foreach (letter; name)
-    {
-        mutable_name ~= letter;
-    }
-
-    mutable_name[0] = 'b';
-
-    writeln("\n", mutable_name);
-
-    // another way is to duplicate the immutable string
-    // into a mutable char[].
-    char[] another_mutable_name = [];
-    another_mutable_name ~= name.dup;
-    another_mutable_name[0] = 'c';
-    writeln(another_mutable_name);
+    // you can also use the strings.dup property to transform
+    // a string to a char[].
+    mutable_char_from_dup();
 }
